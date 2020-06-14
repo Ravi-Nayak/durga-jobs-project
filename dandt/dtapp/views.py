31423c638d2f08ea135090a069f9ef9b450a3992
unknown <you@example.com>
2020-06-14 20:00:51 +0530

    firstcommit

---
+++ dandt/dtapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+
+# Create your views here.
+def date_time_view(request):
+    date=datetime.datetime.now()
+    s='<h1>the current date and time at server is :' + str(date) +'</h1>'
+    return HttpResponse(s)
+
+def index(request):
+    return render(request,'index.html')
